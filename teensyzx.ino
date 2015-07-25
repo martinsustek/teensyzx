570929196801e372f704cfe49df6aab8e41fbebd
Martin <martin@Jakub-Mac-mini.local>
2015-07-25 14:48:29 +0200

    optimalizace video, zatim bez borderu a barev - 7.5ms/pulframe => 22.5% vykonova rezerva

--- teensyzx.ino
+++ teensyzx.ino
@@ -20,13 +20,6 @@ const int ledPin = 13;
 
 int cycles_emulated;
 UTFT myDisplay;
-union {
-  uint8_t b8[512];
-  uint16_t b16[256];
-} display_line_buffer;
-
-int millis_A;
-int millis_B;
 
 void display_init() {
   myDisplay = UTFT(ILI9325D_8, DISPLAY_RS, DISPLAY_WR, DISPLAY_CS, DISPLAY_RST);
@@ -34,45 +27,54 @@ void display_init() {
   myDisplay.clrScr();
 }
 
-inline void display_draw_bitmap_fast(int x, int y) {
-  unsigned int col;
-  int tx;
-
-  int millis_0, millis_1, millis_2;
-
-  myDisplay.setXY(x, y, x+256-1, y);
-  sbi(myDisplay.P_RS, myDisplay.B_RS);
-  for (tx=512-1; tx>=0; tx--) {
-    *(volatile uint8_t *)(&GPIOD_PDOR) = display_line_buffer.b8[tx];
-    pulse_low(myDisplay.P_WR, myDisplay.B_WR);
-  } 
-}
-
-inline void display_draw_line(uint8_t line_number) {
-  uint16_t line_start = (((line_number % 8) << 3) | (((line_number / 8) % 8)) | ((line_number / 64) << 6)) * 32;
-  for (int x = 0; x < 32; x++) {
-    uint16_t fg_color = 0x0000;
-    uint16_t bg_color = 0xFFFF;
-    uint8_t mask = 0b10000000;
-    for (int i = 0; i < 8; i++) {
-      if (RAM[line_start + x] & mask) {
-        display_line_buffer.b16[x * 8 + i] = fg_color;
-      } else {
-        display_line_buffer.b16[x * 8 + i] = bg_color;
-      }
-      mask = mask >> 1;
-    }
-  }
-  //myDisplay.drawBitmap((320 - 256) / 2, (240 - 192) / 2 + line_number, 256, 1, display_line_buffer);
-  display_draw_bitmap_fast(32, 24 + line_number);
-}
+uint8_t display_draw_start_line = 0;
 
 inline void display_draw() {
-  cbi(myDisplay.P_CS, myDisplay.B_CS);
-  for (int line_number = 0; line_number < 192; line_number++) {
-    display_draw_line(line_number);
+  //cbi(myDisplay.P_CS, myDisplay.B_CS);
+  digitalWriteFast(DISPLAY_CS, LOW);
+  for (int line_number = display_draw_start_line; line_number < 192; line_number += 2) {
+    myDisplay.setXY(32, 24 + line_number, 32+256-1, 24 + line_number);
+    //sbi(myDisplay.P_RS, myDisplay.B_RS);
+    digitalWriteFast(DISPLAY_RS, HIGH);
+  
+    uint16_t line_start = (((line_number % 8) << 3) | (((line_number / 8) % 8)) | ((line_number / 64) << 6)) * 32;
+    for (int x = 31; x >= 0; x--) {
+      //uint16_t fg_color = 0x0000;
+      //uint16_t bg_color = 0xFFFF;
+      uint8_t mini_line = RAM[line_start + x];
+      uint8_t mask = 0b00000001;
+      for (uint8_t i = 0; i < 8; i++) {
+        if (mini_line & mask) {
+          *(volatile uint8_t *)(&GPIOD_PDOR) = 0x00;
+          //pulse_low(myDisplay.P_WR, myDisplay.B_WR);
+          digitalWriteFast(DISPLAY_WR, LOW);
+          __asm__("nop");
+          digitalWriteFast(DISPLAY_WR, HIGH);
+          *(volatile uint8_t *)(&GPIOD_PDOR) = 0x00;
+          //pulse_low(myDisplay.P_WR, myDisplay.B_WR);
+          digitalWriteFast(DISPLAY_WR, LOW);
+          __asm__("nop");
+          digitalWriteFast(DISPLAY_WR, HIGH);
+        } else {
+          *(volatile uint8_t *)(&GPIOD_PDOR) = 0xFF;
+          //pulse_low(myDisplay.P_WR, myDisplay.B_WR);
+          digitalWriteFast(DISPLAY_WR, LOW);
+          __asm__("nop");
+          digitalWriteFast(DISPLAY_WR, HIGH);
+          *(volatile uint8_t *)(&GPIOD_PDOR) = 0xFF;
+          //pulse_low(myDisplay.P_WR, myDisplay.B_WR);
+          digitalWriteFast(DISPLAY_WR, LOW);
+          __asm__("nop");
+          digitalWriteFast(DISPLAY_WR, HIGH);
+        }
+        mask = mask << 1;
+      }
+    }
   }
-  sbi(myDisplay.P_CS, myDisplay.B_CS);
+  //sbi(myDisplay.P_CS, myDisplay.B_CS);
+  digitalWriteFast(DISPLAY_CS, HIGH);
+
+  display_draw_start_line ^= 1;
 }
 
 
@@ -98,15 +100,13 @@ void loop() {
   //delay(950);
 
   //Serial.printf("Emulating... ");
-  for (int i = 0; i < 25; i++) {
-    millis_A = 0;
-    millis_B = 0;
+  for (int i = 0; i < 50; i++) {
     millis0 = millis();
-    cycles_emulated += Z80Emulate(&state, 140000);
+    cycles_emulated += Z80Emulate(&state, 70000);
     millis1 = millis();
     display_draw();
     millis2 = millis();
-    Serial.printf("Emulation took %d ms, rendering took %d ms (A %d, B %d), total %d ms.\n", millis1 - millis0, millis2 - millis1, millis_A, millis_B, millis2 - millis0);
+    Serial.printf("Emulation took %d ms, rendering took %d ms, total %d ms.\n", millis1 - millis0, millis2 - millis1, millis2 - millis0);
   }
 
   //Serial.printf("cycles emulated: %d\n", cycles_emulated);
